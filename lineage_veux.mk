49a6c8335fd4b7558929c4968e4800f7c25ec156
lect <ryhyouki@gmail.com>
2023-02-22 03:38:40 +0530

    veux: add more infos to the makefile

--- lineage_veux.mk
+++ lineage_veux.mk
@@ -10,6 +10,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
